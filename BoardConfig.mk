a0ea84818a76d4bc563517e98bfe1660c7950126
Joe Maples <joe@maples.dev>
2022-04-11 15:43:28 -0400

    fix: Remove some kernel config options
    Change-Id: Idb6defe2b834ba1a206a0b17c5db0ef81268a9b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,10 +39,6 @@ PRODUCT_HAVE_OPTEE := false
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
-    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := rk3566_eink_defconfig
