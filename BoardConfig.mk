51721b6454c88c166c52cb23ce47e09e75fc5b98
Joe Maples <joe@maples.dev>
2022-04-30 20:22:33 -0400

    Use pinenote dts and parameter

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
     TARGET_RECOVERY_FSTAB := device/rockchip/rk356x/rk3566_r/recovery.fstab_AB
 endif
 PRODUCT_UBOOT_CONFIG := rk3566-eink
-PRODUCT_KERNEL_DTS := rk3566-rk817-eink-w103
+PRODUCT_KERNEL_DTS := rk3566-rk817-eink-pinenote
 PRODUCT_FSTAB_TEMPLATE := device/rockchip/rk356x/rk3566_eink/fstab_eink.in
 
 BOARD_USE_LAZY_HAL := true
